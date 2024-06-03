732ec23303f82158af76dbc6b3ed4f403ae2f969
febry10 <42637504+febry10@users.noreply.github.com>
2024-06-04 00:56:06 +0700

    Add files via upload

---
+++ anime/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+$host = "localhost"; // Nama hostnya
+$user = "root"; // Username
+$pass = ""; // Password (Isi jika menggunakan password)
+$database = "kuningan"; // Nama databasenya
+$connect = mysqli_connect($host, $user, $pass, $database); // Koneksi ke MySQL
+
+?>
